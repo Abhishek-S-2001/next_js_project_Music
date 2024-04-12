19cbbac1145365ca83d1d37d17f65e090c3e93fe
Abhishek Shekhawat <85601866+Abhishek-S-2001@users.noreply.github.com>
2024-04-12 12:08:11 +0530

    Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import TestimonialCards from "@/components/TestimonialCards";
-import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -11,7 +10,6 @@ export default function Home() {
       <FeaturedCourses />
       <WhyChooseUs />
       <TestimonialCards />
-      <UpcomingWebinars />
     </main>
   );
 }
